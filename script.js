57015ede496b3d12b3b8ec8d3f80ae8b8da38bb8
trishamullapudi-12 <trishamullapudi@gmail.com>
2026-02-07 09:11:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select navbar
+const navbar = document.getElementById('navbar');
+
+// Change navbar style on scroll
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
